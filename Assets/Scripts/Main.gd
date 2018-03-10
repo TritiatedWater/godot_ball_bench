8e27ec75f3b994cddfafe84df7563056d0c3d9ce
Supatier <atisuperati@gmail.com>
2018-03-10 12:13:26 +0700

    Add avg fps and  0.1%

--- Assets/Scripts/Main.gd
+++ Assets/Scripts/Main.gd
@@ -5,17 +5,64 @@ extends Spatial
 # var b = "textvar"
 var fps_label = null
 var var_label = null
+var time_start = 0
+var time_now = 0
+var fps = 0
+var frame_time = float()
+
+var start_tick = 0
+var cur_tick = 0
+var prev_tick
+var elapsed_tick = 0
+var tick_arr = []
+var fps_arr = []
+
+var avg_fps = float()
+var prev_fps
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	OS.set_window_fullscreen(true)
 	fps_label = get_node("Panel/FPS")
 	
+	time_start = OS.get_unix_time()
+	start_tick = OS.get_ticks_msec()
+	prev_tick = start_tick
+	set_process(true)
+	
 func _process(delta):
 	fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
+	fps += 1
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		get_tree().quit()
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
+		
+	time_now = OS.get_unix_time()
+	var elapsed = time_now - time_start
+	
+	if ((elapsed - prev_tick) != 0):
+		fps_arr.append(Engine.get_frames_per_second())
+		
+	prev_fps = elapsed
+	cur_tick = OS.get_ticks_msec()
+	tick_arr.append(cur_tick - prev_tick)
+	prev_tick = cur_tick
+	
+	if (elapsed > 360):
+		avg_fps = (fps / elapsed)
+		elapsed_tick = cur_tick - start_tick
+		frame_time = elapsed_tick / fps
+		tick_arr.sort()
+		fps_arr.sort()
+		print("Elapsed Time: ", elapsed, " seconds")
+		print("Avg FPS: ", fps / elapsed)
+		print("AVG Frame Time: ", frame_time , " milliseconds per FPS")
+		print(tick_arr[(0.99 * fps)])
+		print(fps_arr[(0.01 * fps)])
+		
+		get_tree().quit()
+		
+		
