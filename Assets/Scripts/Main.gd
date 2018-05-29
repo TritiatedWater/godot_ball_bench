db2d08eabcdc2d716989718264791cc19ddd2938
Supatier <atisuperati@gmail.com>
2018-05-29 08:33:57 +0700

    Update Main.gd

--- Assets/Scripts/Main.gd
+++ Assets/Scripts/Main.gd
@@ -1,3 +1,8 @@
+"""
+Copyright 2018 Supatier
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/."""
 extends Node
 
 var fps_label = null
